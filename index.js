7e5dbafb0195c1218dbbb5e7d8ea65946a91ce16
Ivan Orkeny <studioanimacio@gmail.com>
2024-11-04 09:09:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -562,13 +562,13 @@ router.get('/receive', async ctx => {
   await sendfile(ctx, 'static/download.html')
 })
 
-router.get('/Books', async ctx => {
+router.get('/', async ctx => {
   const agent = ctx.get('user-agent')
   console.log(ctx.ip, agent)
   await sendfile(ctx, agent.includes('Kobo') || agent.includes('Kindle') || agent.toLowerCase().includes('tolino') || agent.includes('eReader') /*"eReader" is on Tolino*/ ? 'static/download.html' : 'static/upload.html')
 })
 
-router.get('/Books:filename', downloadFile)
+router.get('/:filename', downloadFile)
 
 app.use(serve("static"))
 
